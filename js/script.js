5519af98c4cb9acd8c868b32843bb3f607a64a7b
vabill <vbill@hawk.iit.edu>
2024-11-12 00:19:48 -0600

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,27 @@
+// for maps.html
+function initMap(){
+	var map = new google.maps.Map(document.getElementById('map'), {
+		center: {lat: 32.857166293767435, lng: -117.25741432029214},
+		zoom: 10,
+		mapTypeID: "satellite",
+	});
+	
+	map.setTilt(45);
+
+	const marker = new google.maps.Marker({
+		position: {lat: 32.857166293767435, lng: -117.25741432029214},
+		map: map,
+		title: "La Jolla Shores Beach",
+		animation: google.maps.Animation.BOUNCE,
+	});
+	
+	const infoWindow = new google.maps.InfoWindow({
+		content: "<h3>La Jolla Shores Beach</h3><p>I was here almost weekly when I lived in Cali!</p>"
+	});
+	
+	marker.addEventListener("click", () => {
+		infoWindow.open(map, marker);
+	});
+}
+
+google.maps.event.addDOMListener(window, 'load', initMap)
